ce49c1e44a79f47d25d6983af924d5872547eeff
smitra0916 <mitrasourasish@gmail.com>
2024-03-26 22:55:42 +0530

    added firebase db

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+// import 'package:flutter/widgets.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
 import 'package:smart_heart/firebase_options.dart';
 
@@ -30,12 +31,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required String title});
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String name = '';
+  String sex = '';
+  int age = 0;
+  int pulse = 0;
+  int spo2 = 0;
+  int cbg = 0;
+  String bp = '';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    DocumentSnapshot snapshot = await FirebaseFirestore.instance
+        .collection('Patients')
+        .doc('dGuo1yzC2ciSvzLGo37p')
+        .get();
+    Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+    setState(() {
+      name = data['Name'];
+      sex = data['Sex'];
+      age = data['Age'];
+      pulse = data['Pulse'];
+      spo2 = data['SPO2'];
+      cbg = data['CBG'];
+      bp = data['BP'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.only(top: 100.0, left: 20),
         child: Column(children: <Widget>[
@@ -43,18 +83,28 @@ class MyHomePage extends StatelessWidget {
             CircleAvatar(
               radius: 90,
             ),
-            SizedBox(width: 20,),
+            SizedBox(
+              width: 20,
+            ),
             Column(
               children: [
                 Row(
                   children: [
-                    Text('NAME: ', style: TextStyle(fontSize: 20),),
-                    Text('name', style: TextStyle(fontSize: 20))],
+                    Text(
+                      'NAME: ',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(name, style: TextStyle(fontSize: 20))
+                  ],
                 ),
                 Row(
                   children: [
-                    Text('AGE: ', style: TextStyle(fontSize: 20),),
-                    Text('age', style: TextStyle(fontSize: 20))],
+                    Text(
+                      sex.toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(age.toString(), style: TextStyle(fontSize: 20))
+                  ],
                 )
               ],
             ),
@@ -64,42 +114,66 @@ class MyHomePage extends StatelessWidget {
             child: Column(
               children: [
                 Padding(
-                  padding: EdgeInsets.only(top:10),
+                  padding: EdgeInsets.only(top: 10),
                   child: Row(
                     children: [
                       Icon(Icons.monitor_heart_rounded),
-                      Text('PULSE:', style: TextStyle(fontSize: 15),),
-                      Text('age', style: TextStyle(fontSize: 15),)
+                      Text(
+                        'PULSE:',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        pulse.toString(),
+                        style: TextStyle(fontSize: 15),
+                      )
                     ],
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top:10),
+                  padding: EdgeInsets.only(top: 10),
                   child: Row(
                     children: [
                       Icon(Icons.monitor_heart_rounded),
-                      Text('SPO2:', style: TextStyle(fontSize: 15),),
-                      Text('age', style: TextStyle(fontSize: 15),)
+                      Text(
+                        'SPO2:',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        spo2.toString(),
+                        style: TextStyle(fontSize: 15),
+                      )
                     ],
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top:10),
+                  padding: EdgeInsets.only(top: 10),
                   child: Row(
                     children: [
                       Icon(Icons.monitor_heart_rounded),
-                      Text('CBG:', style: TextStyle(fontSize: 15),),
-                      Text('age', style: TextStyle(fontSize: 15),)
+                      Text(
+                        'CBG:',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        cbg.toString(),
+                        style: TextStyle(fontSize: 15),
+                      )
                     ],
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top:10),
+                  padding: EdgeInsets.only(top: 10),
                   child: Row(
                     children: [
                       Icon(Icons.monitor_heart_rounded),
-                      Text('BP:', style: TextStyle(fontSize: 15),),
-                      Text('age:', style: TextStyle(fontSize: 15),)
+                      Text(
+                        'BP:',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        bp,
+                        style: TextStyle(fontSize: 15),
+                      )
                     ],
                   ),
                 )
