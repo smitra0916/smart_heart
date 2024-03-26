e8d20f5df2eb55143fcd98ee1eff1d3f7d65232e
smitra0916 <mitrasourasish@gmail.com>
2024-03-26 19:24:47 +0530

    added patient details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,112 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {
+import 'package:smart_heart/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return (MaterialApp(
       title: 'Smart Heart',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Smart Heart'),
-    );
+    ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<String> data;
-
-   Future<String> fetchData() async {
-  final response = await http.get(Uri.parse('https://api.ipify.org/?format=json'));
-
-  if (response.statusCode == 200) {
-    // If the server returns a 200 OK response, then parse the JSON.
-    Map<String, dynamic> jsonData = jsonDecode(response.body);
-    String data = jsonData['ip'];
-    print(data);
-    return data;
-  } else {
-    // If the server returns an unsuccessful response code, throw an exception.
-    throw Exception('Failed to load data');
-  }
-}
-
-  @override
-  void initState() {
-    super.initState();
-    data = fetchData();
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required String title});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FutureBuilder<String>(
-          future: data,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text('Data: ${snapshot.data}');
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
-        ),
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 100.0, left: 20),
+        child: Column(children: <Widget>[
+          Row(children: <Widget>[
+            CircleAvatar(
+              radius: 90,
+            ),
+            SizedBox(width: 20,),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Text('NAME: ', style: TextStyle(fontSize: 20),),
+                    Text('name', style: TextStyle(fontSize: 20))],
+                ),
+                Row(
+                  children: [
+                    Text('AGE: ', style: TextStyle(fontSize: 20),),
+                    Text('age', style: TextStyle(fontSize: 20))],
+                )
+              ],
+            ),
+          ]),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top:10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.monitor_heart_rounded),
+                      Text('PULSE:', style: TextStyle(fontSize: 15),),
+                      Text('age', style: TextStyle(fontSize: 15),)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.monitor_heart_rounded),
+                      Text('SPO2:', style: TextStyle(fontSize: 15),),
+                      Text('age', style: TextStyle(fontSize: 15),)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.monitor_heart_rounded),
+                      Text('CBG:', style: TextStyle(fontSize: 15),),
+                      Text('age', style: TextStyle(fontSize: 15),)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.monitor_heart_rounded),
+                      Text('BP:', style: TextStyle(fontSize: 15),),
+                      Text('age:', style: TextStyle(fontSize: 15),)
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
